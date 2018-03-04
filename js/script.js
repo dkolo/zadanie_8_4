6ae6f6b9050e2e4a9d0ec980e6347952faf77968
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-03-04 17:55:04 +0100

    Add comments in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert('test');
+// This is javascript
+
+var variable = 'test';
+
+console.log(variable);
+
+// alert alert('test');
+
+/*
+	Kiedyś wpiszę tutaj jak wyglądałby powyższy kod w dogescript
+*/
